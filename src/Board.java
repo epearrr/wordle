401f87a8f373f54cd777927b5eea788dc54eefa2
epearrr <etahonp@gmail.com>
2022-07-18 15:25:58 -0400

    switched from characters to strings

--- src/Board.java
+++ src/Board.java
@@ -4,35 +4,87 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Board {
+    private String word;
+    private String guess;
     private ArrayList<List<CellStatus>> statusLayout;
-    private ArrayList<List<Character>> charsLayout;
+    private ArrayList<List<String>> lettersLayout;
 
-    public Board() {
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+
+    public Board(String word) {
+        this.word = word;
+        this.guess = "     ";
+
         statusLayout = new ArrayList<>();
-        charsLayout = new ArrayList<>();
+        lettersLayout = new ArrayList<>();
 
         CellStatus[] statuses = new CellStatus[5];
         Arrays.fill(statuses, CellStatus.UNGUESSED);
         List<CellStatus> statusList = Arrays.asList(statuses);
 
-        Character[] characters = new Character[5];
-        Arrays.fill(characters, "_");
-        List<Character> charList = Arrays.asList(characters);
+        String[] Strings = new String[5];
+        Arrays.fill(Strings, "_");
+        List<String> letterList = Arrays.asList(Strings);
 
         for(int i = 0; i < 5; i++) {
             statusLayout.add(statusList);
-            charsLayout.add(charList);
+            lettersLayout.add(letterList);
         }
     }
 
-    // 
+    /**
+     * takes a guess made by the user and adds the guess to the next available row in the lettersLayout
+     * @param guess
+     */
     public void makeGuess(String guess) {
+        this.guess = guess;
+        int layoutIndex = 0;
 
+        // find the next available row in lettersLayout
+        while(statusLayout.get(layoutIndex).get(0) != CellStatus.UNGUESSED) {
+            layoutIndex++;
+        }
+
+        // iterates through each character in the guess and adds it to the lettersLayout
+        for(int i = 0; i < 5; i++) {
+            lettersLayout.get(layoutIndex).set(i, guess.substring(i, i+1));
+            System.out.println("lettersLayout is " + lettersLayout);
+        }
     }
 
+    /**
+     * @return lettersLayout
+     */
+    public ArrayList<List<String>> getLettersLayout() {
+        return lettersLayout;
+    }
+
+    /**
+     * @return statusLayout
+     */
+    public ArrayList<List<CellStatus>> getStatusLayout() {
+        return statusLayout;
+    }
+
+
+    /**
+     * @return a visual representation of the Wordle board
+     */
     @Override
     public String toString() {
-        
-        return String.format();
+        String visualBoard = "";
+        String ansi_start = "";
+        String ansi_end = "";
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                visualBoard += ansi_start + lettersLayout.get(i).get(j) + ansi_end;
+                visualBoard += " ";
+            }
+            visualBoard += "\n"; 
+        }
+        return String.format(visualBoard + "\n" + word);
     }
 }
